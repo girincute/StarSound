e0d07271c1b7dd6ca7e713f539af774c040aa0af
getsolaris <getsolaris@naver.com>
2018-08-19 04:29:13 +0900

    front first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import { BrowserRouter as Route } from 'react-router-dom';
+import Home from './views/Home';
+
+class App extends Component {
+  render() {
+    return (
+      <Route>
+        <Home />
+      </Route>
+    );
+  }
+}
+
+export default App;
---
+++ src/views/Home.js
@@ -0,0 +1,240 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+import styled, { injectGlobal } from 'styled-components';
+import SearchIcon from '../images/s2.png';
+import JuaFont from '../font/jua.ttf'
+
+injectGlobal`
+  @font-face {
+    font-family: 'Jua';
+    src: url(${JuaFont});
+  }
+
+  body {
+    font-family: 'Jua' ,sans-serif;
+    padding: 0;
+    margin: 0;
+    background-color: #e6ecf0;
+    overflow-x: hidden
+  }
+`
+const Navbar = styled.div`
+  text-align:center;
+  height: 53px;
+  box-shadow: 0 15px 50px 0 rgba(213,216,228,.3);
+  background-color: white;
+  padding-top: 12px;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
+  
+`;
+
+const SearchButton = styled.input`
+  position:relative;
+  left:10px;
+  width: 570px;
+  height: 40px;
+  display:inline-block;
+  border: none;
+  text-align: center;
+  font-size: 21px;
+  background: url(${SearchIcon}) no-repeat scroll 2px 2px;
+  background-position-y: 12px;
+  background-position-x: 20px;
+  background-color: #e6ecf0;
+  
+`;
+
+const PostBoxContainer = styled.div`
+  width: 570px;
+  margin:40px auto;
+`;
+
+const Section = styled.section`
+  background-color: white;
+  box-shadow: rgba(213, 216, 228, 0.3) 0px 15px 50px 0px;
+  height: inherit;
+  text-align: left;
+  border-width: 1px;
+  border-style: solid;
+  border-color: rgba(237, 237, 237, 0.2);
+  border-image: initial;
+  margin: 5em auto;
+  font-weight: 200
+`;
+
+const WriteBox = styled.textarea`
+  width: 545px;
+  height: 160px;
+  border: none;
+  padding: 10px;
+  resize: none;
+  font-size: 15px;
+  display: block;
+  &:focus {
+    outline: none
+  }
+  
+`;
+
+const Header = styled.div`
+  font-weight: bold;
+  height: 20px;
+  padding-left: 30px;
+  padding:10px;
+  border-bottom: 1px solid rgba(237, 237, 237, 0.8);
+  font-weight: 200;
+  
+`;
+const Bottom = styled.div`
+  height: 30px;
+  margin: 0 auto;
+  padding-left: 30px;
+  padding:10px;
+  text-align:right;
+  border-top: 1px solid rgba(237, 237, 237, 0.8);
+`;
+
+const TimeBottom = styled.div`
+height: 30px;
+margin: 0 auto;
+padding-left: 30px;
+padding:10px;
+text-align:center;
+border-top: 1px solid rgba(237, 237, 237, 0.8);
+`;
+
+const PostBox = styled.div`
+  width: 545px;
+  padding: 10px;
+  background-color: rgb(255, 255, 255);
+  box-shadow: rgba(213, 216, 228, 0.3) 0px 15px 50px 0px;
+  height: inherit;
+  text-align: left;
+  border-width: 1px;
+  border-style: solid;
+  border-color: rgba(237, 237, 237, 0.2);
+  border-image: initial;
+  margin: 0 auto;
+`;
+
+const WriteButton = styled.button`
+  background-color: rgb(82,158,204);
+  color: white;
+  width: 80px;
+  height: 35px;
+  border: none;
+  font-weight: bold
+`;
+
+const Title = styled.p`
+  font-size: 30px;
+  margin-top: -5px;
+  font-weight: 200;
+`
+
+const LikeButton = styled.button`
+  border: none
+`;
+
+
+const server = 'http://youngseo.crong.kr:3000/'
+export default class Home extends Component {
+  state = {
+    posts: [],
+    loading: true,
+    post: ''
+  }
+
+  componentDidMount = (e) => {
+    axios.get(server + 'board')
+    .then(res => {
+      const posts = res.data;
+      this.setState({
+        posts, loading: false
+      })
+    })
+  }
+
+  handleChange = (e) => {
+    this.setState({ post: e.target.value })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const post = {
+      title: '',
+      description: this.state.post,
+    }
+
+    axios.post(server + 'board', post, {
+      headers: {
+          'Content-Type': 'application/json',
+    }})
+    .then(res => {
+      console.log(res)
+      window.location.reload()
+    })
+  }
+
+  handleLikeClick = (e) => {
+    console.log('like click')
+    axios.get(server + 'board/' + e.target.value + '/like')
+    .then(res => {
+      console.log('좋아요 완료')
+      window.location.reload()
+    })
+  }
+
+  handleScrollTop = (e) => {
+    window.scrollTo(0, 0)
+  }
+
+  render() {
+    let postData = [...this.state.posts]
+    postData.sort((a,b) => b.board_idx - a.board_idx)
+    return (
+      <div>
+        <Navbar>
+          <SearchButton placeholder="검색"/>
+        </Navbar>
+        <Container>
+          <PostBoxContainer>
+            <Section>
+              <Header>
+                <Title>별소리</Title>
+              </Header>
+            <form onSubmit={this.handleSubmit}>
+              <WriteBox placeholder="사연을 입력 해주세요" onChange={this.handleChange}/>
+              <Bottom>
+              <WriteButton>포스팅</WriteButton>
+              </Bottom>
+            </form>
+            </Section>
+
+            <Section>
+            <Header>
+                기록들 ..
+            </Header>
+              { 
+                this.state.loading ? 'loading.. ' : 
+                postData.map((post) =>
+                <PostBox key={post.board_idx}><pre>{post.board_des}</pre><LikeButton onClick={this.handleLikeClick} value={post.board_idx}>♡ {post.board_hit}</LikeButton></PostBox>)
+              }
+              <TimeBottom onClick={this.handleScrollTop}>
+                위로...
+              </TimeBottom>
+              </Section>
+          </PostBoxContainer>
+        </Container>
+      </div>
+    )
+  }
+}
